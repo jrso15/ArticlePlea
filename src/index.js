9767b83f98a5380f96715b7b7361b65e908c7474
jrso15 <janilleolegario@gmail.com>
2021-08-20 19:45:20 +0800

    for rendering - WIP

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Option1 from "./components/Option1/Option1";
+import Option2 from "./components/Option2/Option2";
+
+// ReactDOM.render(<Option1 />, document.getElementById("root"));
+ReactDOM.render(<Option2 />, document.getElementById("root"));
