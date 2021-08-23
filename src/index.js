773faa548ed797df6b71e4c651fcd13997d1c4f1
jrso15 <janilleolegario@gmail.com>
2021-08-23 13:43:03 +0800

    class config

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@ import ReactDOM from "react-dom";
 import Option1 from "./components/Option1/Option1";
 import Option2 from "./components/Option2/Option2";
 
-// ReactDOM.render(<Option1 />, document.getElementById("root"));
-ReactDOM.render(<Option2 />, document.getElementById("root"));
+const container = document.createElement("div");
+document.body.appendChild(container);
+
+global.ArticlePlea = class ArticlePlea {
+  constructor(config) {
+    this.config = { ...config };
+  }
+
+  init(type) {
+    type = this.config.type.toLowerCase();
+
+    if (type === "option1") {
+      ReactDOM.render(<Option1 />, container);
+    } else if (type === "option2") {
+      ReactDOM.render(<Option2 />, container);
+    } else {
+      console.log("Error! Unknown config type.");
+      return;
+    }
+  }
+};
+
+// ReactDOM.render(<Option2 />, document.getElementById("root"));
