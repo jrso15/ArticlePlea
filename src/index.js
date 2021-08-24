fe8384eb52380f8fc2ee01ccf373e12a27149a46
jrso15 <janilleolegario@gmail.com>
2021-08-24 15:24:00 +0800

    change div to container

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Option1 from "./components/Option1/Option1";
 import Option2 from "./components/Option2/Option2";
 
 let container = document.createElement("div");
-div.classList.add("Row-sc-1t49lz9-0 eGnIar");
+container.classList.add("Row-sc-1t49lz9-0 eGnIar");
 
 document
   .querySelector(".ArticleWrapper__ArticleBodyWrapper-sc-36pn73-0")
