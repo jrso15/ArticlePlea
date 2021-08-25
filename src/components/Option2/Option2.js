e45897b534d329eb356f20a7f8af89e8aa5acc2a
jrso15 <janilleolegario@gmail.com>
2021-08-25 16:37:24 +0800

    new parameter for logo

--- src/components/Option2/Option2.js
+++ src/components/Option2/Option2.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ArticlePlea from "../AbstractArticlePlea/AbstractArticlePlea";
-import image from "./glenda.png";
+import logo from "./mixed.png";
 import styles from "./Option2.module.scss";
 
 const Option2 = () => {
   return (
     <ArticlePlea
-      //   image={image}
       title="Our online world is broken."
       paragraph1="The battle is no longer about access, but about who takes hold of the narrative. Despite the abundance of information, we can no longer discuss freely without fear of getting trolled, threatened, or attacked. Our minds have been co-opted by the attention economy, and our deepest vulnerabilities have been exploited for profit through disinformation and propaganda."
       paragraph2="In this climate and as we prepare for the May 2022 presidential election in the Philippines, we need stories that provide clarity, context and perspective. Stories that understand how power is used and abused. Stories that drive communities to action."
       paragraph3="Rappler is powered by its readers. It is independent and fearless because it’s supported and inspired by you"
       btnLabel="Join Rappler+"
       btnSubLabel="for P3500 / $75 a year"
+      rapplerPlusLogo={logo}
       optionStyle={styles}
     />
   );
