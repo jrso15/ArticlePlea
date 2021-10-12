1d5559dc93d3a7fe706014c033c4ffc13ea8c4e7
jrso15 <janilleolegario@gmail.com>
2021-10-12 15:28:49 +0800

    changed to italic text

--- src/components/Option2/Option2.js
+++ src/components/Option2/Option2.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactHtmlParser from "react-html-parser";
 import ArticlePlea from "../AbstractArticlePlea/AbstractArticlePlea";
 import logo from "./white_1.png";
 import styles from "./Option2.module.scss";
@@ -23,14 +24,26 @@ const Option2 = () => {
       "https://r3.rappler.com/plus/payment?campaign=3&utm_source=rappler_plus&utm_medium=article_plea&utm_campaign=option2_crowdfunding"
     );
   };
+
+  const italicText =
+    "<em> Without truth, you can’t have trust. Without trust, we have no shared reality. </em>";
+
+  const italicText2 =
+    "<em> What does a world without a shared reality look like? Globally, It becomes impossible to deal with our existential problems. For communities, it means they can no longer engage in constructive discourse because dissenters are tagged as terrorists or criminals. For individuals, it means feeling more alone, anxious, disconnected, and disempowered. </em>";
+
+  const italicText3 =
+    "<em>Our vision for Rappler+ is to create a place where we could keep talking to each other about the future we want to shape. A place where we could engage in conversation without the toxicity and vitriol on social media. An opportunity to deepen our relationship with our readers and supporters in a space where we could discuss today’s most pressing issues – together.</em>";
+
+  const italicText4 = "<em>Courage on. Hold the line</em>";
+
   return (
     <ArticlePlea
       title="Ride the wave of freedom"
       paragraph1="Rappler CEO Maria Ressa invites you to join Rappler+ and engage in a global discourse on democracy, independent media, human rights, and civic engagement."
-      paragraph2="Without truth, you can’t have trust. Without trust, we have no shared reality."
-      paragraph3="What does a world without a shared reality look like? Globally, It becomes impossible to deal with our existential problems. For communities, it means they can no longer engage in constructive discourse because dissenters are tagged as terrorists or criminals. For individuals, it means feeling more alone, anxious, disconnected, and disempowered."
-      paragraph4="Our vision for Rappler+ is to create a place where we could keep talking to each other about the future we want to shape. A place where we could engage in conversation without the toxicity and vitriol on social media. An opportunity to deepen our relationship with our readers and supporters in a space where we could discuss today’s most pressing issues – together."
-      donation="Courage on. Hold the line"
+      paragraph2={ReactHtmlParser(italicText)}
+      paragraph3={ReactHtmlParser(italicText2)}
+      paragraph4={ReactHtmlParser(italicText3)}
+      donation={ReactHtmlParser(italicText4)}
       btnLabel="Join our community"
       btnSubLabel="for P3500 / $75 a year"
       rapplerPlusLink={handleLink}
